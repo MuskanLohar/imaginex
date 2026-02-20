8d7e2e9bfd0332bb48484d61271a677a8cef5598
muskan lohar <muskanlohar0@gamil.com>
2026-02-20 18:27:17 +0530

    jwt token update

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,61 +1,78 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/userModel.js"
 
-const registerUser = async(req, res) => {
+const registerUser = async (req, res) => {
 
     const { name, email, phone, password } = req.body
 
     // Check if all fields are coming
-    if(!name || !email || !phone || !password){
+    if (!name || !email || !phone || !password) {
         res.status(409)
         throw new Error('Please Fill All Details!')
     }
 
     // Check if user already exists
-    let emailExist = await User.findOne({email : email})
-      let phoneExist = await User.findOne({phone : phone})
+    let emailExist = await User.findOne({ email: email })
+    let phoneExist = await User.findOne({ phone: phone })
 
-      if(emailExist || phoneExist){
+    if (emailExist || phoneExist) {
         res.status(409)
         throw new Error("User Already Exists!")
-      }
+    }
 
 
-      // Hash password
-       const salt = bcrypt.genSaltSync(10);
-       const hashedPassword = bcrypt.hashSync(password, salt);
+    // Hash password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
 
 
 
 
-      // Register User
-      let user = await User.create({ name, email, phone, password: hashedPassword })
+    // Register User
+    let user = await User.create({ name, email, phone, password: hashedPassword })
 
-      if(!user){
+    if (!user) {
         res.status(400)
         throw new Error("User Not Created!")
-      }
+    }
 
-      res.status(201).json(user)
-
-   
+    res.status(201).json({
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        isAdmin: user.isAdmin,
+        isActive: user.isActive,
+        credits: user.credits,
+        token: generateToken(user._id)
+    })
 }
 
-const loginUser = async(req, res) => {
+const loginUser = async (req, res) => {
     const { email, password } = req.body
 
     // Check if all fields are coming
-    if( !email || !password ){
+    if (!email || !password) {
         res.status(409)
         throw new Error('Please Fill All Details!')
     }
 
-     // Check if user exists
-    let user = await User.findOne({ email : email })
+    // Check if user exists
+    let user = await User.findOne({ email: email })
 
-    if(user && await bcrypt.compare(password, user.password)){
-        res.status(200).json(user)
-    }else{
+    if (user && await bcrypt.compare(password, user.password)) {
+        res.status(200).json({
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+            isAdmin: user.isAdmin,
+            isActive: user.isActive,
+            credits: user.credits,
+            token: generateToken(user._id)
+        })
+    } else {
         res.status(400)
         throw new Error("inavlid credentials!")
     }
@@ -63,6 +80,23 @@ const loginUser = async(req, res) => {
 }
 
 
-const authController = { registerUser, loginUser }
+// protected Controller
+const privateController = (req, res) => {
+
+    console.log(req.user)
+
+     res.send("i am private controller " + req.user.name)
+}
+
+
+
+
+// Generate Token
+const generateToken = (id) => {
+
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' })
+}
+
+const authController = { registerUser, loginUser, privateController }
 
 export default authController
